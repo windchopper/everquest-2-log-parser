8cb92e6d08c6ba06b7743a074aa9d414e8db7404
wind <wind@comtv.ru>
2021-04-05 02:31:04 +0300

    #

---
+++ run-linux.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -classpath target/lib/kotlin/*:target/lib/kotlin/ext/*:target/lib/javafx/*:target/lib/javafx/linux/*:target/lib/*:target/* com.github.windchopper.tools.everquest.log.parser.ApplicationKt
